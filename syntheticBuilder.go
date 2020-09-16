71e766682f6cb3483bd04d5258c7cdca9ea228ba
Daniel Martí <mvdan@mvdan.cc>
2020-09-16 16:16:26 +0100

    skeleton builder and prototype

--- syntheticBuilder.go
+++ syntheticBuilder.go
@@ -1,3 +1,25 @@
 package hamt
 
+import "github.com/ipld/go-ipld-prime"
+
 // TODO : the synthetic NodeBuilder + NodeAssembler that act like a single unified map go here
+
+var _ ipld.NodeBuilder = (*builder)(nil)
+
+type builder struct {
+}
+
+func (b *builder) Build() ipld.Node { return nil }
+func (b *builder) Reset() {}
+
+func (b *builder) BeginMap(sizeHint int) (ipld.MapAssembler, error) { return nil, nil }
+func (b *builder) BeginList(sizeHint int) (ipld.ListAssembler, error) { return nil, nil }
+func (b *builder) AssignNull() error { return nil }
+func (b *builder) AssignBool(bool) error { return nil }
+func (b *builder) AssignInt(int) error { return nil }
+func (b *builder) AssignFloat(float64) error { return nil }
+func (b *builder) AssignString(string) error { return nil }
+func (b *builder) AssignBytes([]byte) error { return nil }
+func (b *builder) AssignLink(ipld.Link) error { return nil }
+func (b *builder) AssignNode(ipld.Node) error { return nil }
+func (b *builder) Prototype() ipld.NodePrototype { return nil }
--- syntheticNode.go
+++ syntheticNode.go
@@ -1,3 +1,24 @@
 package hamt
 
+import "github.com/ipld/go-ipld-prime"
+
 // TODO : the synthetic Node that acts like a single unified map goes here, and so does its Prototype and whatever Config that entails.
+
+// var _ ipld.Node = (*Node)(nil)
+
+type Node struct {
+	// TODO(mvdan): shouldn't these be in the schema?
+	hashAlg    string
+	bucketSize int
+
+	_InteriorNode
+}
+
+var _ ipld.NodePrototype = (*Prototype)(nil)
+
+type Prototype struct {
+}
+
+func (Prototype) NewBuilder() ipld.NodeBuilder {
+	return &builder{}
+}
